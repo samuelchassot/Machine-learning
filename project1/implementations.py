b8c1dddf721f1f9f8f4d3d20adfb8d5f5fb360b3
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2019-10-07 14:04:07 +0200

    add functions prototypes in implementations.py

---
+++ project1/implementations.py
@@ -0,0 +1,18 @@
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    return NotImplementedError
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    return NotImplementedError
+
+def least_squares(y, tx):
+    return NotImplementedError
+
+def ridge_regression(y, tx, lambda_):
+    return NotImplementedError
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    return NotImplementedError
+
+def reg_logistic_regression(y, tx, lambda_, initial_w,
+        max_iters, gamma):
+    return NotImplementedError
