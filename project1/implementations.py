correct ridge regression formula

project1/implementations.py
@@ -42,7 +42,7 @@ def least_squares(y, tx):
 
 def ridge_regression(y, tx, lambda_):
     w = np.linalg.inv(tx.T @ tx +
-            lambda_*np.eye(tx.shape[1]) @ (tx.T @ y)
+            lambda_*2*len(y)*np.eye(tx.shape[1]) @ (tx.T @ y)
     return w, compute_loss(y , tx, w)
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):