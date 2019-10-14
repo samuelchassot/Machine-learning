612d2d809fa984c250c1efdbc2055a77b994ce80
Julie Giunta <julie.giunta@epfl.ch>
2019-10-14 15:00:11 +0200

    Code for checking which columns are useless

--- project1/implementations.py
+++ project1/implementations.py
@@ -1,23 +1,4 @@
 import numpy as np
-import random
-
-
-def standardize(x):
-    mean = np.mean(x)
-    std = np.std(x)
-
-    return (x-mean)/std, mean, std
-
-def get_batches(y, tx, num_batches):
-    seed = random.randint(0,1000000)
-    np.random.seed(seed)
-    tx_shuffle = np.random.shuffle(tx)
-    y_shuffle = np.random.shuffle(y)
-
-    for i in range(num_batches):
-        end_indx = min(i+1, len(y))
-        if i != end_indx:
-            yield y_shuffle[i: end_indx], tx_shuffle[i: end_indx]
 
 def mse(e):
     return 1/2*np.mean(e**2)
@@ -48,11 +29,12 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
 
 def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     w = initial_w
-    for y_batch, tx_batch in get_batches(y, tx, max_iters):
+    for y_batch, tx_batch in batch_iter(y, tx, 1, max_iters, True):
         gradient, error = compute_gradient(y_batch, tx_batch, w)
         loss = mse(error)
         w = w - gamma * gradient
-    return w, loss
+
+   return w, loss
 
 def least_squares(y, tx):
     w = np.linalg.inv(tx.T @ tx) @ (tx.T @ y)
@@ -60,8 +42,8 @@ def least_squares(y, tx):
 
 def ridge_regression(y, tx, lambda_):
     w = np.linalg.inv(tx.T @ tx +
-            lambda_*2*len(y)*np.eye(tx.shape[1]) @ (tx.T @ y))
-    return w, compute_rmse_loss(y, tx, w)
+            lambda_*2*len(y)*np.eye(tx.shape[1]) @ (tx.T @ y)
+    return w, compute_loss(y , tx, w)
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
     return NotImplementedError
